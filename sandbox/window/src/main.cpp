bb06b52c158bbe0ede26cdb0530726d67eef9234
Joren <joren.hnns@gmail.com>
2020-11-20 15:02:18 +0100

    Use the new terminal printing function in the window sandbox project

--- sandbox/window/src/main.cpp
+++ sandbox/window/src/main.cpp
@@ -2,13 +2,18 @@
 
 #include <glad/glad.h>
 
+#include <core/debug/terminal.h>
 #include <core/window.h>
 
 using namespace caverneer;
 
 int main()
 {
-    createWindow(720, 480, "Hello, World!");
+    if (!createWindow(720, 480, "Hello, World!"))
+    {
+        debugPrint("Window creation failed!");
+        return 1;
+    }
 
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
     while (!shouldWindowClose())
